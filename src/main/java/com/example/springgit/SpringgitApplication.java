98eabe5e3fb1d6cf94ce7890d8da3de19612351b
Ramshad <ramshad.picacod@gmail.com>
2025-06-25 10:09:35 +0530

    first commit

---
+++ src/main/java/com/example/springgit/SpringgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springgit/SpringgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
